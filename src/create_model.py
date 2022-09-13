3c4b2c5369e3b9ca74cf1d93f77c1160f5e6c67d
cam <cjohnson23@gwu.edu>
2022-09-13 12:34:16 -0400

    changes

--- src/create_model.py
+++ src/create_model.py
@@ -1,16 +1,16 @@
 import tensorflow as tf
 from keras.models import Model
 from keras.layers import Input, Conv1D, BatchNormalization, Activation, MaxPooling1D, \
-                         AveragePooling1D, Add, GRU, Dense, Permute, Reshape, Dropout
+                         AveragePooling1D, Add, Bidirectional, LSTM, Dense, Permute, Reshape, Dropout
 from keras.regularizers import l2
 from keras.utils.vis_utils import plot_model
 
-def create_model(input_shape=(625, 1), lr=0.0001, plot=False):
+def create_model(input_shape=(625, 1), lr=0.0001, decay=0.0001, plot=False):
     input = Input(shape=input_shape)
     output = activebp(input, lr=lr)
     model = Model(inputs=input, outputs=output)
 
-    optimizer = tf.keras.optimizers.RMSprop(learning_rate=lr, decay=0.0001)
+    optimizer = tf.keras.optimizers.RMSprop(learning_rate=lr, decay=decay)
     model.compile(optimizer=optimizer,
                   loss='mse',
                   metrics=['mae'])
@@ -20,8 +20,10 @@ def create_model(input_shape=(625, 1), lr=0.0001, plot=False):
     return model
 
 def activebp(x, lr):
-    x1 = resnet50(x)
-    x1 = GRU(65)(x1)
+    x1 = resnet(x)
+    x1 = Bidirectional(LSTM(64,
+                            activation='tanh',
+                            recurrent_activation='sigmoid'))(x1)
     x1 = BatchNormalization()(x1)
 
     x2 = spectro_temporal_block(x)
@@ -34,31 +36,31 @@ def activebp(x, lr):
     output = Dense(2, activation='relu')(output)
     return output
 
-def resnet50(x):
-    x = Conv1D(kernel_size=(7), filters=64, strides=(2))(x)
+def resnet(x):
+    x = Conv1D(kernel_size=(7), filters=64, strides=(2), padding='same')(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
     x = MaxPooling1D((3), strides=(2))(x)
 
-    x = convolutional_block(x, kernel_sizes=[(1), (3), (1)], filters=[64, 64, 256])
-    x = identity_block(x, kernel_sizes=[(1), (3), (1)], filters=[64, 64, 256])
-    x = identity_block(x, kernel_sizes=[(1), (3), (1)], filters=[64, 64, 256])
+    x = convolutional_block(x, kernel_sizes=[(3), (3)], filters=[64, 64])
+    x = identity_block(x, kernel_sizes=[(3), (3)], filters=[64, 64])
+    x = identity_block(x, kernel_sizes=[(3), (3)], filters=[64, 64])
 
-    x = convolutional_block(x, kernel_sizes=[(1), (3), (1)], filters=[128, 128, 512])
-    x = identity_block(x, kernel_sizes=[(1), (3), (1)], filters=[128, 128, 512])
-    x = identity_block(x, kernel_sizes=[(1), (3), (1)], filters=[128, 128, 512])
-    x = identity_block(x, kernel_sizes=[(1), (3), (1)], filters=[128, 128, 512])
+    x = convolutional_block(x, kernel_sizes=[(3), (3)], filters=[128, 128])
+    x = identity_block(x, kernel_sizes=[(3), (3)], filters=[128, 128])
+    x = identity_block(x, kernel_sizes=[(3), (3)], filters=[128, 128])
+    x = identity_block(x, kernel_sizes=[(3), (3)], filters=[128, 128])
 
-    x = convolutional_block(x, kernel_sizes=[(1), (3), (1)], filters=[256, 256, 1024])
-    x = identity_block(x, kernel_sizes=[(1), (3), (1)], filters=[256, 256, 1024])
-    x = identity_block(x, kernel_sizes=[(1), (3), (1)], filters=[256, 256, 1024])
-    x = identity_block(x, kernel_sizes=[(1), (3), (1)], filters=[256, 256, 1024])
-    x = identity_block(x, kernel_sizes=[(1), (3), (1)], filters=[256, 256, 1024])
-    x = identity_block(x, kernel_sizes=[(1), (3), (1)], filters=[256, 256, 1024])
+    x = convolutional_block(x, kernel_sizes=[(3), (3)], filters=[256, 256])
+    x = identity_block(x, kernel_sizes=[(3), (3)], filters=[256, 256])
+    x = identity_block(x, kernel_sizes=[(3), (3)], filters=[256, 256])
+    x = identity_block(x, kernel_sizes=[(3), (3)], filters=[256, 256])
+    x = identity_block(x, kernel_sizes=[(3), (3)], filters=[256, 256])
+    x = identity_block(x, kernel_sizes=[(3), (3)], filters=[256, 256])
 
-    x = convolutional_block(x, kernel_sizes=[(1), (3), (1)], filters=[512, 512, 2048])
-    x = identity_block(x, kernel_sizes=[(1), (3), (1)], filters=[512, 512, 2048])
-    x = identity_block(x, kernel_sizes=[(1), (3), (1)], filters=[512, 512, 2048])
+    x = convolutional_block(x, kernel_sizes=[(3), (3)], filters=[512, 512])
+    x = identity_block(x, kernel_sizes=[(3), (3)], filters=[512, 512])
+    x = identity_block(x, kernel_sizes=[(3), (3)], filters=[512, 512])
 
     x = AveragePooling1D(pool_size=(2), padding='same')(x)
     return x
@@ -66,10 +68,8 @@ def resnet50(x):
 def convolutional_block(x, kernel_sizes, filters):
     kernel_size0 = kernel_sizes[0]
     kernel_size1 = kernel_sizes[1]
-    kernel_size2 = kernel_sizes[2]
     filter0 = filters[0]
     filter1 = filters[1]
-    filter2 = filters[2]
 
     x_skip = x
 
@@ -81,11 +81,7 @@ def convolutional_block(x, kernel_sizes, filters):
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
 
-    x = Conv1D(kernel_size=kernel_size2, filters=filter2, padding='same')(x)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-
-    x_skip = Conv1D(kernel_size=kernel_size2, filters=filter2, padding='same')(x_skip)
+    x_skip = Conv1D(kernel_size=kernel_size1, filters=filter1, padding='same')(x_skip)
     x_skip = BatchNormalization()(x_skip)
 
     x = Add()([x, x_skip])
@@ -95,10 +91,8 @@ def convolutional_block(x, kernel_sizes, filters):
 def identity_block(x, kernel_sizes, filters):
     kernel_size0 = kernel_sizes[0]
     kernel_size1 = kernel_sizes[1]
-    kernel_size2 = kernel_sizes[2]
     filter0 = filters[0]
     filter1 = filters[1]
-    filter2 = filters[2]
 
     x_skip = x
 
@@ -107,10 +101,6 @@ def identity_block(x, kernel_sizes, filters):
     x = Activation('relu')(x)
 
     x = Conv1D(kernel_size=kernel_size1, filters=filter1, padding='same')(x)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-
-    x = Conv1D(kernel_size=kernel_size2, filters=filter2, padding='same')(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
 
@@ -128,6 +118,8 @@ def spectro_temporal_block(x):
                        frame_step=frame_step)
     x = tf.abs(x)
     x = Reshape((63, 65))(x)
-    x = GRU(65)(x)
+    x = Bidirectional(LSTM(64,
+                           activation='tanh',
+                           recurrent_activation='sigmoid'))(x)
     x = BatchNormalization()(x)
     return x
