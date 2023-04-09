f197623b1e764cc37a713ddf9f61f6689474e24f
camhpj <cjohnson23@gwu.edu>
2023-04-09 17:11:27 -0400

    updated testing notebook

--- scripts/train.py
+++ scripts/train.py
@@ -5,7 +5,7 @@ repo_dir = os.getcwd().split('notebooks')[0]
 os.chdir(repo_dir)
 
 worker = TrainingPipeline(
-    config_path='/home/cam/Documents/database_tools/data/mimic3-data-20230407/config.ini',
+    config_path='/home/cam/Documents/database_tools/data/mimic3-data-20230408/config.ini',
     no_sweep=True,
 )
 worker.run()
